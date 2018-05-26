9da41e4a2e6a7d3d73396e5963198f8a958533ed
tutunak <dk@tutunak.com>
2018-05-26 18:18:21 +0300

    simple test db

---
+++ tests/test_t_image_pub.py
@@ -0,0 +1,22 @@
+import unittest
+import t_image_pub
+
+
+class TestAddImages(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+class TestCleanImagesPublished(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+class TestPublicImage(unittest.TestCase):
+    def setUp(self):
+        pass
+
+
+
+if __name__ == '__main__':
+    unittest.main()
