3b4460304073095ecc8f3c94fc55b098367c853d
tutunak <dk@tutunak.com>
2018-05-26 18:17:04 +0300

    settings example

---
+++ settings_example.py
@@ -0,0 +1,7 @@
+TOKEN = ''          # api token, more information in
+                    # https://telegram.me/botfather
+CHANNEL = ''        # chanel name for publication
+IMAGES_PATH = ''    # images path
+DATABASE_NAME = ''  # sqlite database name
+
+
